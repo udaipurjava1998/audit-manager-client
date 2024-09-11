390f0e89e7dee37e544f74777d5b12d771c52ccb
Azhardeen <azharudeen.nazeerdeen@acnovate.com>
2024-09-11 11:38:36 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import { useContext } from 'react';
 import './App.css';
-import config from './config';
-import { AuthContext } from './context/AuthContext';
-import Login from './layouts/authentication/Login';
 import Header from './layouts/header/Header';
 
 function App() {
